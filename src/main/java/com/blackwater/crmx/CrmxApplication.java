4eeb7991a4e31be48a94118476a6a6da4eab3f04
Charlie <charlie@blackwater.gg>
2024-10-06 05:55:30 +0100

    Spring boilerplate

---
+++ src/main/java/com/blackwater/crmx/CrmxApplication.java
@@ -0,0 +1,13 @@
+package com.blackwater.crmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackwater/crmx/CrmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackwater.crmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
